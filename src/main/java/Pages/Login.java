202070d84764c3f93c9fd942be3de79925f6c341
Swati <swatithombare@gmail.com>
2025-08-21 16:40:55 +0530

    First Commit

--- src/main/java/Pages/Login.java
+++ src/main/java/Pages/Login.java
@@ -1,4 +1,41 @@
 package Pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class Login {
+
+    @FindBy (id="username")
+    WebElement textUsername;
+
+    @FindBy (id="password")
+    WebElement Textpassword;
+
+    @FindBy (xpath="//button[normalize-space()='Login to QKart']")
+    WebElement loginbtn;
+
+
+    public Login (WebDriver driver)
+    {
+
+        PageFactory.initElements(driver, this);
+    }
+
+    public void setTextUsername(String username){
+        textUsername.sendKeys(username);
+    }
+
+    public void setTextPassword(String passwordn){
+        Textpassword.sendKeys(passwordn);
+    }
+
+    public void setloginbtn(){
+        loginbtn.click();
+    }
+
+
+
+
 }
--- src/test/java/regression/LoginTest.java
+++ src/test/java/regression/LoginTest.java
@@ -1,4 +1,24 @@
 package regression;
 
+import Pages.Login;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class LoginTest {
+
+
+    @Test
+    public void loginTest()
+    {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://crio-qkart-frontend-qa.vercel.app/login");
+
+        Login login = new Login(driver);
+        login.setTextUsername("test@QKART");
+        login.setTextPassword("MyQKARTapp@1234");
+        login.setloginbtn();
+
+    }
 }
